087a37aaa2668a8f5016a98df1dc6491c29a8b6b
88095 <greenfoot89@gmail.com>
2018-12-05 14:06:37 +0000

    Q4d solution for 5/12

---
+++ src/WinningNumbers.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WinningNumbers {
+    public static void main(String[] args) throws IOException
+    {
+        Boolean lotteryNumbers[] = new Boolean[6];    //Boolean array
+        int numberOfSelectedBalls = 0;
+        int winningBall;
+
+        do {
+            winningBall = (int) ((Math.random() * 50) + 1);
+            if (!lotteryNumbers[winningBall]) {
+                lotteryNumbers[winningBall] = true;
+                numberOfSelectedBalls++;
+            }
+        } while (numberOfSelectedBalls != 6);
+
+        FileWriter fw = new FileWriter("U:\\Com Sci\\Database\\MyFile.txt", true);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        for(int i = 0; i <= 50; i++) {
+            if (lotteryNumbers[i])
+                fw.write(i + "\t");
+            fw.write('\n');
+            fw.close();
+        }
+    }
+}
